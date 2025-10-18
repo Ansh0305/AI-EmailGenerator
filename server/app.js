4309f29124d0614514020c8d710c791aee9ab24e
Ansh0305 <saiansh2016@gmail.com>
2025-10-19 00:09:52 +0530

    OpenAI free limit has completed need alternative from openai

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
-const emailRouter = require('./routes/emailRoutes')
+const emailRouter = require("./routes/emailRoutes");
+
+// Middleware
+app.use(express.json());
 
 // Routes
-app.use('/api', emailRouter);
-
+app.use("/api", emailRouter);
 
 module.exports = app;
--- server/config/openaiConfig.js
+++ server/config/openaiConfig.js
@@ -1,6 +1,7 @@
 const OpenAi = require("openai");
+require("dotenv").config();
 
-const openai = new openai.OpenAi({
+const openai = new OpenAi({
   apiKey: process.env.OPENAI_API_KEY,
 });
 module.exports = openai;
