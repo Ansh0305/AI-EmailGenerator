1a6dde5c3e5dba6fa5205a82c9e5e306f232c938
Ansh0305 <saiansh2016@gmail.com>
2025-10-19 22:37:06 +0530

    Added onclick, onchange and logic to InputForm.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import {AlertCircle, CheckCircle, Copy, Mail, Sparkles, Wand2} from "lucide-react"
-import Header from './components/Header'
-import InputForm from './components/InputForm'
-import ToneSelection from './components/ToneSelection'
-import OutputSelection from './components/OutputSelection'
+import Header from "./components/Header";
+import InputForm from "./components/InputForm";
+import ToneSelection from "./components/ToneSelection";
+import OutputSelection from "./components/OutputSelection";
 
 export default function App() {
+  const [rawThoughts, setRawThoughts] = useState("");
+  const [replyingTo, setReplyingTo] = useState("");
+  const [Tone, setTone] = useState("professional");
+  const [generateEmail, setGenerateEmail] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [showReplySection, setShowReplySection] = useState(false);
+  const [error, setError] = useState("");
+
   return (
-    <div className='min-h-screen bg-gradient-to-br from-blue-50 via-white to-slate-50'>
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-slate-50">
       {/* Header */}
-      <Header/>
+      <Header />
 
       {/* Main Content */}
-      <div className='max-w-6xl mx-auto px-6 py-12'>
-        <div className='grid lg:grid-cols-2 gap-8'>
+      <div className="max-w-6xl mx-auto px-6 py-12">
+        <div className="grid lg:grid-cols-2 gap-8">
           {/* Input Section */}
-          <InputForm/>
+          <InputForm
+            rawThoughts={rawThoughts}
+            replyingTo={replyingTo}
+            setRawThoughts={setRawThoughts}
+            setReplyingTo={setReplyingTo}
+            showReplySection={showReplySection}
+            setShowReplySection={setShowReplySection}
+          />
 
           {/* Tone Selection  */}
-          <ToneSelection/>
+          <ToneSelection />
         </div>
 
-
         {/* Output */}
-        <OutputSelection/>
-
+        <OutputSelection />
       </div>
-    
     </div>
-  )
+  );
 }
