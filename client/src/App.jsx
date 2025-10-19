d3c36bc1e8e09560cb49bd3738c06eb89f42a7f4
Ansh0305 <saiansh2016@gmail.com>
2025-10-19 22:20:43 +0530

    Used the components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import {Mail} from "lucide-react"
+import {AlertCircle, CheckCircle, Copy, Mail, Sparkles, Wand2} from "lucide-react"
 import Header from './components/Header'
+import InputForm from './components/InputForm'
+import ToneSelection from './components/ToneSelection'
+import OutputSelection from './components/OutputSelection'
 
 export default function App() {
   return (
     <div className='min-h-screen bg-gradient-to-br from-blue-50 via-white to-slate-50'>
       {/* Header */}
       <Header/>
+
+      {/* Main Content */}
+      <div className='max-w-6xl mx-auto px-6 py-12'>
+        <div className='grid lg:grid-cols-2 gap-8'>
+          {/* Input Section */}
+          <InputForm/>
+
+          {/* Tone Selection  */}
+          <ToneSelection/>
+        </div>
+
+
+        {/* Output */}
+        <OutputSelection/>
+
+      </div>
     
     </div>
   )
