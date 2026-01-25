e4e4b45cb9dd747f6212919041ef468425e576f6
Ansh0305 <saiansh2016@gmail.com>
2026-01-25 17:39:15 +0530

    Spelling mistake

--- server/middlewares/notFoundMiddleware.js
+++ server/middlewares/notFoundMiddleware.js
@@ -1,5 +1,5 @@
 exports.notFound = (req, res, next) => {
-  const error = new Error(`Not found - ${req.originakUrl}`);
+  const error = new Error(`Not found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
